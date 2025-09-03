944a25108c7176d2c358208047bd8a0c8c41bc28
rootminusone8004 <rootminusone8004@gmail.com>
2025-09-03 21:07:55 +0000

    added terraform scripts
    -- structured and modular
     -- suited for kubeadm setup

---
+++ terraform/datasources.tf
@@ -0,0 +1,20 @@
+data "aws_ami" "debian_ami" {
+  most_recent = true
+  owners      = ["136693071363"]
+
+  filter {
+    name   = "name"
+    values = ["debian-13-amd64-*"]
+  }
+}
+
+data "aws_ami" "ubuntu_ami" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+}
+
